9d964cfe7e00a4bce3d19608e61ed0c5672e7ada
Ingrid <ingrid.bazin31@gmail.com>
2018-07-08 16:05:02 -0700

    fix: Remove ad dependencies in paid flavor

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -55,19 +55,8 @@ public class MainActivityTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
 
-        ViewInteraction appCompatButton = onView(
-                allOf(withText("Tell Joke"),
-                        childAtPosition(
-                                allOf(withId(R.id.fragment),
-                                        childAtPosition(
-                                                withId(android.R.id.content),
-                                                0)),
-                                1),
-                        isDisplayed()));
-        appCompatButton.perform(click());
-
+        onView(withId(R.id.joke_button)).perform(click());
         onView(withId(R.id.joke_text_view)).check(matches(not(withText(""))));
-
     }
 
     @After
@@ -76,25 +65,4 @@ public class MainActivityTest {
             idlingRegistry.unregister(idlingResource);
         }
     }
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
-    }
-
-
 }
--- jokeLib/src/main/java/com/example/jokejavalib/JokeTeller.java
+++ jokeLib/src/main/java/com/example/jokejavalib/JokeTeller.java
@@ -6,11 +6,13 @@ import java.util.Random;
 public class JokeTeller {
 
     private static String[] jokes = new String[]{
-            "Joke de mon oncle",
-            "Joke de ma tante",
-            "Joke du grand-pere",
-            "Joke de la voisine",
-            "Joke du patron"};
+            "My computer suddenly started belting out \"Someone Like You.\" It's a Dell.",
+            "I had a joke about Neymar, but it doesn't stand",
+            "Q: What do you call a bear with no teeth? A: Gummybear",
+            "Q: Where did the sheep go on vacation? A: The baaaahamas",
+            "How do you stop squirrels playing football in the garden? \n" +
+                    "Hide the ball, it drives them nuts."
+};
 
    public static String getAJoke(){
        Random random = new Random();
